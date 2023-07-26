42385a66901d3c71c2260e450ee9828efd933145
BirajMainali <biraj.prathamit@gmail.com>
2023-07-26 21:48:18 +0545

    init commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using Htmx;
+using Microsoft.AspNetCore.Mvc;
+using htmx_first_run.Models;
+
+namespace htmx_first_run.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index(ProductCreateModel vm)
+    {
+        return Request.IsHtmx() ? PartialView("_IndexPartial", vm) : View(vm);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
+public class ProductCreateModel
+{
+    [Display(Name = "Product Name")] public string Name { get; set; }
+
+    [Display(Name = "Product Price")] public decimal Price { get; set; }
+
+    [Display(Name = "Description")]
+    public string Description { get; set; }
+}
---
+++ Dockerfile
@@ -0,0 +1,20 @@
+﻿FROM mcr.microsoft.com/dotnet/aspnet:7.0 AS base
+WORKDIR /app
+EXPOSE 80
+EXPOSE 443
+
+FROM mcr.microsoft.com/dotnet/sdk:7.0 AS build
+WORKDIR /src
+COPY ["htmx-first-run.csproj", "./"]
+RUN dotnet restore "htmx-first-run.csproj"
+COPY . .
+WORKDIR "/src/"
+RUN dotnet build "htmx-first-run.csproj" -c Release -o /app/build
+
+FROM build AS publish
+RUN dotnet publish "htmx-first-run.csproj" -c Release -o /app/publish /p:UseAppHost=false
+
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+ENTRYPOINT ["dotnet", "htmx-first-run.dll"]
